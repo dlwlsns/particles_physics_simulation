677f4bddb411ce7cdb66ef76893ec0348a8c9a24
Davide Ravani <ravani.davide@outlook.com>
2025-04-10 06:32:58 +0200

    Little code and files cleanup

--- engine/Source/Engine/cg_engine.h
+++ engine/Source/Engine/cg_engine.h
@@ -4,17 +4,7 @@
 #define LIB_NAME      "CG Engine v0.1a"  ///< Library credits
 #define LIB_VERSION   10                 ///< Library version (divide by 10)
 
-#ifdef _WINDOWS 	
-   // Export API:
-   // Specifies i/o linkage (VC++ spec):
-   #ifdef CG_ENGINE_EXPORTS
-		#define LIB_API __declspec(dllexport)
-   #else
-		#define LIB_API __declspec(dllimport)
-   #endif      	
-#else // Under Linux
-   #define LIB_API  // Dummy declaration
-#endif
+#define LIB_API  // Dummy declaration
 
 // GLM
 #include <glm/glm.hpp>
--- engine/Source/Engine/object.h
+++ engine/Source/Engine/object.h
@@ -3,17 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#ifdef _WINDOWS 	
-	// Export API:
-	// Specifies i/o linkage (VC++ spec):
-	#ifdef CG_ENGINE_EXPORTS
-		#define LIB_API __declspec(dllexport)
-	#else
-		#define LIB_API __declspec(dllimport)
-	#endif      	
-	#else // Under Linux
-		#define LIB_API  // Dummy declaration
-#endif
+#define LIB_API  // Dummy declaration
 
 class LIB_API Object
 {
